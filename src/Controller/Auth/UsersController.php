be48bea7ec38fe509e52fa434048e84715bbec24
AnthonyATC1 <anthony.caltagirone@promsocatc.net>
2026-02-12 13:32:18 +0100

    page 52

--- src/Controller/Auth/UsersController.php
+++ src/Controller/Auth/UsersController.php
@@ -9,46 +9,89 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\User;
 use App\Entity\Role;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
-
 class UsersController extends AbstractController
 {
     #[Route('/account/new', name: 'account')]
-    public function accountForm(Request $request, EntityManagerInterface $manager): Response
+    #[Route('/account/edit/{id}', name: 'account-edit')]
+    public function accountForm(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $user = new User();
+        $update = false;
 
-        $form = $this->createFormBuilder($user)
-            ->add('username', TextType::class, [
-                'attr' => [
-                    'placeholder' => 'login'
-                ]
-            ])
-            ->add('password', PasswordType::class, [
-                'attr' => [
-                    'placeholder' => 'password'
-                ]
-            ])
-            ->add('mail', EmailType::class, [
-                'attr' => [
-                    'placeholder' => 'xxx@mail.com'
-                ]
-            ])
+        if ($request->attributes->get('_route') === 'account-edit') {
+
+            $filter = $request->getSession()->get('filter');
+            $idRoleUser = $filter['idRole'];
+            $idUser = $filter['idUser'];
+
+            if ($idUser == $request->attributes->get('id') || $idRoleUser == 1) {
+
+                $user = $entityManager
+                    ->getRepository(User::class)
+                    ->find($request->attributes->get('id'));
+
+                if (!$user) {
+                    return $this->redirectToRoute('index');
+                }
+
+                $idRole = $user->getRole()->getId();
+
+            } else {
+                return $this->redirectToRoute('index');
+            }
+
+            $update = true;
+
+        } else {
+            $user = new User();
+        }
+
+        $userForm = $this->createFormBuilder($user)
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('mail', EmailType::class)
             ->add('role', EntityType::class, [
                 'class' => Role::class,
                 'choice_label' => 'name',
             ])
             ->getForm();
 
+        if ($update) {
+            $userForm->remove('password');
+        }
+
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+
+            $user = $userForm->getData();
+
+            if (!$update) {
+                $passHashed = password_hash($user->getPassword(), PASSWORD_BCRYPT);
+                $user->setPassword($passHashed);
+            } else {
+                if ($user->getRole() === null) {
+                    $role = $entityManager
+                        ->getRepository(Role::class)
+                        ->find($idRole);
+
+                    $user->setRole($role);
+                }
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
 
         return $this->render('view/auth/account.html.twig', [
-            'formAccount' => $form->createView()
+            'formAccount' => $userForm->createView(),
+            'editMode' => $update
         ]);
     }
-
 }
